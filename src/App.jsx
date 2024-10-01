477ddaec78ce82f392dbf47b8945381eac3db663
Serenay Ercan <95965609+serenayercan@users.noreply.github.com>
2024-10-01 16:59:15 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
+import Section from './components/Section.jsx';
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <main>
+      <CoreConcepts/>
+      <Examples />
+      <Section/>
+
+      </main>
+      </>
+  );
+}
+
+ 
+export default App;
